829409a8e44a7a9642478ebe64b017e6dd916559
shrissttyyy <shiristis8@gmail.com>
2025-03-25 05:23:39 +0545

    Update Project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+app.listen(3000, () => {
+    console.log("message")
+})
+
+app.get("/", (req,res) => {
+    res.send("message");
+});
+
+app.post("/", (req,res) => {
+    req.send("message");
+})
